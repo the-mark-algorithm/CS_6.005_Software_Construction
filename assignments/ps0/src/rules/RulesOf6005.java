eb6f13c9909c6dcae3e861c3b29a1f10026a0700
Mark Ono <markeo.py@gmail.com>
2025-01-12 15:32:02 -0500

    Fix logic for mayUseCodeInAssignment.

--- assignments/ps0/src/rules/RulesOf6005.java
+++ assignments/ps0/src/rules/RulesOf6005.java
@@ -36,7 +36,7 @@ public class RulesOf6005 {
             boolean availableToOthers, boolean writtenAsCourseWork,
             boolean citingYourSource, boolean implementationRequired) {
 
-        return writtenByYourself || ((availableToOthers || citingYourSource) && !writtenAsCourseWork);
+        return writtenByYourself || ((availableToOthers && citingYourSource) && !writtenAsCourseWork);
     }
     
     /**
